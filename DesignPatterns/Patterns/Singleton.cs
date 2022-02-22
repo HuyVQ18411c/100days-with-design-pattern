f53d73a0fd5cfadcd62f16c605f63097c3af7f9f
HuyVu-Dev <vuquanghuy@gmail.com>
2022-02-22 23:11:26 +0700

    Add Singleton design pattern

---
+++ DesignPatterns/Patterns/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private List<string> nameList = new();
+        private Singleton()
+        {
+        }
+
+        public static Singleton getInstance()
+        {
+            if(Singleton.instance is null)
+            {
+                Singleton.instance = new Singleton();
+            }
+            return Singleton.instance;
+        }
+
+        public List<string> getNameList()
+        {
+            return this.nameList;
+        }
+    }
+}
